1746219f5d76c3f10617f82f6ac65ebcebcb641e
Mihai Valcu <valcu@orn.mpg.de>
2024-05-08 07:22:06 +0200

    chore: Refactor flagpts function to include additional parameters and improve code readability

--- R/interactive.R
+++ R/interactive.R
@@ -1,15 +1,22 @@
 #' flagpts
 #' Flag outliers interactively
-#' @param d a DT returned by argos_prepare()
-#' @param R point radius
+#' @param dat a DT returned by argos_prepare()
+#' @param R point radius passed to leafltet::addCircleMarkers. 
+#' @param max_speed the initial maximum speed (km/h) used as a selection criterion.
+#' @param fix_lon translate longitude so that it works with leaflet
 #' @export
 #' @examples
 #' x <- dbq(q = "Select * from ARGOS.2018_PESA where tagID ='52753' ") |> argos_prepare()
-
 #' flagpts(x)
-flagpts <- function(d, R = 10) {
+
+flagpts <- function(dat, R = 10, max_speed = 500, fix_lon = TRUE) {
+  
+  d = copy(dat)
   speed_along(d)
 
+  if(fix_lon)
+    d[, longitude := (longitude + 360) %% 360]
+  
   if (!"flag" %in% names(d)) d[, flag := NA]
   d[is.na(flag), flag := 0]
 
@@ -23,25 +30,33 @@ flagpts <- function(d, R = 10) {
     absolutePanel(
       id = "controls", fixed = TRUE,
       draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto", width = 330, height = "auto",
-      sliderInput("maxspeed", "Max speed (kmh)", 50, 500, 300)
+      sliderInput(
+        inputId = "maxspeed", 
+        label = "Max speed (kmh)", 
+        min   = 50, 
+        max   = 500, 
+        value = max_speed)
     )
   )
 
 
   server <- function(input, output) {
+
+    onStop(function() assign("flagged_points", d[(flag), pk], envir = .GlobalEnv))
+
     output$map <- renderLeaflet({
       leaflet() |>
         addProviderTiles("Esri.WorldGrayCanvas") |>
         addGeodesicPolylines(data = dlines) |>
         addCircleMarkers(
-          data = d,
-          lng = ~longitude,
-          lat = ~latitude,
-          fillColor = ~col,
-          color = NA,
-          radius = R,
+          data        = d,
+          lng         = ~longitude,
+          lat         = ~latitude,
+          fillColor   = ~col,
+          color       = NA,
+          radius      = R,
           fillOpacity = .75,
-          layerId = ~pk
+          layerId     = ~pk
         )
     })
 
@@ -70,16 +85,18 @@ flagpts <- function(d, R = 10) {
       leafletProxy("map") |>
         clearMarkers() |>
         clearShapes() |>
-        addGeodesicPolylines(data = xlines) |>
+        addGeodesicPolylines(
+          data   = xlines, 
+          weight = 2.5) |>
         addCircleMarkers(
-          data = d,
-          lng = ~longitude,
-          lat = ~latitude,
-          fillColor = ~col,
-          color = NA,
-          radius = R,
-          fillOpacity = .75,
-          layerId = ~pk
+          data        = d,
+          lng         = ~longitude,
+          lat         = ~latitude,
+          fillColor   = ~col,
+          color       = NA,
+          radius      = R,
+          fillOpacity = 0.75,
+          layerId     = ~pk
         )
     })
   }
